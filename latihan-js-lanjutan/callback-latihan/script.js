78cf56250d42bbc6e3b52e24d0e29321595748d0
aikazm <zakiamelia@gmail.com>
2020-04-03 22:04:30 +0700

    fetch refactor (async await)

--- latihan-js-lanjutan/callback-latihan/script.js
+++ latihan-js-lanjutan/callback-latihan/script.js
@@ -36,36 +36,77 @@
 
 // fetch
 
-const searchButton = document.querySelector(".search-button");
-searchButton.addEventListener("click", function() {
-  const inputKeyword = document.querySelector(".input-keyword");
-  fetch("http://www.omdbapi.com/?apikey=44254820&s=" + inputKeyword.value)
-    .then(response => response.json())
-    .then(response => {
-      const movies = response.Search;
-      let cards = "";
-      movies.forEach(m => (cards += showCards(m)));
-      const movieContainer = document.querySelector(".movie-container");
-      movieContainer.innerHTML = cards;
+// const searchButton = document.querySelector(".search-button");
+// searchButton.addEventListener("click", function() {
+//   const inputKeyword = document.querySelector(".input-keyword");
+//   fetch("http://www.omdbapi.com/?apikey=44254820&s=" + inputKeyword.value)
+//     .then(response => response.json())
+//     .then(response => {
+//       const movies = response.Search;
+//       let cards = "";
+//       movies.forEach(m => (cards += showCards(m)));
+//       const movieContainer = document.querySelector(".movie-container");
+//       movieContainer.innerHTML = cards;
 
-      // click button
-      const modalDetailButton = document.querySelectorAll(
-        ".modal-detail-button"
-      );
-      modalDetailButton.forEach(btn => {
-        btn.addEventListener("click", function() {
-          const imdbid = this.dataset.imdbid;
-          fetch("http://www.omdbapi.com/?apikey=44254820&i=" + imdbid)
-            .then(response => response.json())
-            .then(m => {
-              const movieDetail = showMovieDetail(m);
-              const modalBody = document.querySelector(".modal-body");
-              modalBody.innerHTML = movieDetail;
-            });
-        });
-      });
-    });
+//       // click button
+//       const modalDetailButton = document.querySelectorAll(
+//         ".modal-detail-button"
+//       );
+//       modalDetailButton.forEach(btn => {
+//         btn.addEventListener("click", function() {
+//           const imdbid = this.dataset.imdbid;
+//           fetch("http://www.omdbapi.com/?apikey=44254820&i=" + imdbid)
+//             .then(response => response.json())
+//             .then(m => {
+//               const movieDetail = showMovieDetail(m);
+//               const modalBody = document.querySelector(".modal-body");
+//               modalBody.innerHTML = movieDetail;
+//             });
+//         });
+//       });
+//     });
+// });
+
+const searchButton = document.querySelector(".search-button");
+searchButton.addEventListener("click", async function() {
+  const inputKeyword = document.querySelector(".input-keyword");
+  const movies = await getMovies(inputKeyword.value);
+  updateUI(movies);
 });
+
+// event binding
+document.addEventListener("click", async function(e) {
+  if (e.target.classList.contains("modal-detail-button")) {
+    const imdbid = e.target.dataset.imdbid;
+    const movieDetail = await getMovieDetail(imdbid);
+    updateUIDetail(movieDetail);
+  }
+});
+
+function getMovieDetail(imdbid) {
+  return fetch("http://www.omdbapi.com/?apikey=44254820&i=" + imdbid)
+    .then(response => response.json())
+    .then(m => m);
+}
+
+function getMovies(keyword) {
+  return fetch("http://www.omdbapi.com/?apikey=44254820&s=" + keyword)
+    .then(response => response.json())
+    .then(response => response.Search);
+}
+
+function updateUIDetail(m) {
+  const movieDetail = showMovieDetail(m);
+  const modalBody = document.querySelector(".modal-body");
+  modalBody.innerHTML = movieDetail;
+}
+
+function updateUI(movies) {
+  let cards = "";
+  movies.forEach(m => (cards += showCards(m)));
+  const movieContainer = document.querySelector(".movie-container");
+  movieContainer.innerHTML = cards;
+}
 
 function showCards(m) {
   return `<div class="col-md-4 my-3">
